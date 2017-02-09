7ed4330a7957acbbbe08d8c03ab954c8f090c48d
rburkhardt <rburkhardt@rodericks-mbp.hsd1.ca.comcast.net>
2017-02-09 10:01:53 -0800

    Initialize

---
+++ src/assignment6/Assignment6.java
@@ -0,0 +1,64 @@
+/* ==================================================================
+*
+*   PROGRAM NAME:
+*       Assignment5 - GUI Cards
+*
+*   Description:
+*	Understand the Classes and Problem
+*	
+*	We wish to move our Card classes from the realm of console apps to 
+*	that of GUI apps.  We'll do this in stages.
+*	
+*	>   Read and display Card pictures - Read .gif image files as Icons, 
+*	and attach them to JLabels that we can display on a JFrame.
+*	
+*	>   Encapsulate the Card Icons in a class GUICard - Once we debug 
+*	imagery for cards, above, we can move it into its own class, GUICard.
+*	
+*	>   Create a CardTable class - This JFrame class will embody the 
+*	JPanels and Layout(s) needed for our application. This is where all 
+*	the cards and controls will be placed. 
+*	
+*	>   Use a CardGameFramework class - Use an already created class to 
+*	deal cards for display from an actual deck.
+*	
+*	>Create the game "High-Card"
+*	
+*	The first phase (item 1) will allow you to debug the problem of reading 
+*	the .gif files and displaying them on a JFrame without any excess logic 
+*	or class complexity.  The second phase (items 2 and 3) will let you 
+*	turn what you did in the first phase into a multi-class project.  The 
+*	final phase (items 4 and 5) will add the CardGameFramework class so 
+*	that your card tools can be combined with your GUI tools to create a 
+*	GUI program that has real computational power for a GUI card game, 
+*	"High Card".
+*
+*   Classes:
+*       Phase I - None
+*       Phase II -
+*           CardTable extends JFrame, GUICard, Card, Hand, Deck
+*
+*   Parameters:
+*       1. none
+*
+*   Additional Files:
+*
+*   Created:
+*       2017/02/01
+*
+*   Author/s:
+*       Faiga Revah, Oswaldo Minez, Roderick Burkhardt
+*
+* ==================================================================*/
+
+
+public class Assignment6
+{
+
+    
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
