4c702c7b05456dbc26d8349d8a1641b60a7c3d1f
rburkhardt <rburkhardt@rodericks-mbp.hsd1.ca.comcast.net>
2017-02-13 20:11:07 -0800

    updates

---
+++ src/Timer.java
@@ -0,0 +1,40 @@
+/* ==================================================================
+*
+*   PROGRAM NAME:
+*       Assignment6 - Timed High-Card Game
+*
+*   Description:
+*	
+*	>   Change the program into a Model-View-Controller Design Pattern. 
+*           
+*	>   Add a new part to the High-Card game by putting a timer on the 
+*           side of the screen.  It will be on a timer to update every second,  
+*           but in order for you to still play the game, you will need to use  
+*           multithreading.  (Timer class) 
+*           
+*	>   Design a new game. 
+*           
+*	>   Redraw the UML diagram so that it represents your new structure.
+*
+*   Classes:
+*       Card, Hand, Deck, GUICard, CardTable extends JFrame, CardGameFramework
+*
+*   Parameters:
+*       1. none
+*
+*   Additional Files:
+*
+*   Created:
+*       2017/02/08
+*
+*   Author/s:
+*       Faiga Revah, Roderick Burkhardt, Oswaldo Minez
+*
+* ==================================================================*/
+
+
+
+public class Timer extends Thread
+{
+    
+}
